63fc4a0f098106bee23ca98df9d68de0752127fb
Nujoma Halls <purplesunset2004@yahoo.com>
2016-10-01 21:31:42 -0400

    Finally got the app working on localhost:3000 !
    I even got the controllers script working for the GET method. This is my
    very first project so even though this is minor, it feels great to me.

--- Footsteps/controllers/users.js
+++ Footsteps/controllers/users.js
@@ -1,36 +1,18 @@
-const express = require('express');
-const models = require('../models');
+var express = require('express');
+var router = express.Router();
 
-module.exports = {
-  registerRouter() {
-    const router = express.Router();
 
-    router.get('/', this.index);
-    router.get('/:username', this.show);
 
-    return router;
-  },
-  index(req, res) {
-    models.User.findAll().then((user) => {
-      res.render('users', {
-        user,
-      });
-    });
-  },
-  show(req, res) {
-    models.User.findOne({
-      where: { username: req.params.username },
-    }).then((user) => {
-      models.Post.findAll({
-        where: { email: user.email },
-      }).then((post) => {
-        res.render('users/single', {
-          user,
-          post,
-        });
-      });
-    }).catch(() => {
-      res.render('users/single');
-    });
-  },
-};
+// define the homepage route
+router.get('/', function(req, res) {
+  res.send('<h1>Welcome to Footsteps</h1>');
+});
+
+
+//define specific page in website
+router.get('/:slug', function(req, res) {
+  res.send('My pet dog is: ' + req.params.slug);
+});
+
+
+module.exports = router;
