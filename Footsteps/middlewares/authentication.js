1f292be3b172144a27d2c3012c620232d899a38b
Nujoma Halls <purplesunset2004@yahoo.com>
2016-12-05 13:13:02 -0500

    login authentication fixed

--- Footsteps/middlewares/authentication.js
+++ Footsteps/middlewares/authentication.js
@@ -1,5 +1,7 @@
-const bcrypt = require('bcrypt-nodejs');
 const passport = require('passport');
+
+const bcrypt = require('bcrypt-nodejs');
+
 const LocalStrategy = require('passport-local').Strategy;
 
 const Users = require('../models').Users;
@@ -12,25 +14,52 @@ function passwordsMatch(passwordSubmitted, storedPassword) {
 passport.use(new LocalStrategy({
   usernameField: 'email',
   },
-  function(email, password, done) {
+  (email, password, done)  => {
     console.log(email, password);
     Users.findOne({
       where: { email },
     })
-    .then(function(users) {
-      if (users) {
-        if (passwordsMatch(password, users.password) === false) {
+    .then((user) => {
+      if (user) {
+        if (passwordsMatch(password, user.password) === false) {
           return done(null, false, { message: 'Incorrect password.' });
         }
       }
-      else if (users === null) {
+      else if (user == null) {
         return done(null, false, { message: 'Incorrect email.' });
       }
 	  console.log ('successfully logged in');
-    return done(null, users, { message: 'Successfully Logged In!' });
+    return done(null, user, { message: 'Successfully Logged In!' });
     });
   })
 );
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  Users.findById(id).then((user) => {
+    if (user == null) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  });
+});
+
+// function isLoggedIn(req,res,next) {
+//   if (req.isAuthenticated())
+//     return next();
+//   res.redirect('/');
+// }
+
+passport.redirectIfLoggedIn = (route) =>
+ (req, res, next) => (req.user ? res.redirect(route) : next());
+
+passport.redirectIfNotLoggedIn = (route) =>
+ (req, res, next) => (req.user ? next() : res.redirect(route));
 
 //passport.redirectIfLoggedIn = (route) =>
 //  (req, res, next) => (req.user ? res.redirect(route) : next());
